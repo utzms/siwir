488fa171bca3c0b8ed2156568a9c0d04dccfc9cd
derwahrezocker <basti_schusi@web.de>
2013-11-27 08:04:04 -0800

    main-testmethod

--- GaussSeidel/main.cpp
+++ GaussSeidel/main.cpp
@@ -3,7 +3,33 @@
 
 int main(int argc, char ** argv)
 {
+        //	cout << "!!!Version 0.01!!!" << endl;
+	int row = 4;
+	int column = 4;
+	Grid Grid(row, column);
+	 
+	//int size = row*column;
+	double counter = 1;
+	for ( int i = 0; i < column; ++i)
+	{
+		for ( int j =0; j < row; ++j)
+		{		
+			Grid.setValue(i,j,counter);
+			
+		       // std::cout << Grid.getValue(i,j) << std::endl;
+			++counter;	
+		}
+	}
 
+	for ( int i = 0;i < column; ++i)
+	{
+		for (int j = 0; j < row; ++j)
+		{
+		if ( Grid.getIndexBlack(i,j) != -1)
+			std::cout << Grid.getValue(i,j) << std::endl;
+		}
+	}
 
-return 0;
+		
+        return 0;
 }
